3d1a1bab5c326d0db54c64d77ea66b4f261f0128
jdcalvelli <jd@pklwrks.xyz>
2024-08-05 16:09:37 -0400

    starting narrative control logic

---
+++ src/config.rs
@@ -0,0 +1,8 @@
+turbo::cfg! {r#"
+	name = "project-sol"
+	version = "1.0.0"
+	author = "folie a deux"
+	description = "write something here eventually"
+	[settings]
+	resolution = [384, 216]
+"#}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,64 @@
-// This is where your main game loop code goes
-// The stuff in this block will run ~60x per sec
-turbo::go! ({
-    text!("Hello, world!!!");
-});
+mod config;
+
+// to get the whole script file
+static SCRIPT_PATH: &str = std::include_str!("test.txt");
+
+turbo::init! {
+    struct GameState {
+        lines: Vec<String>,
+        current_line: usize,
+    } = {
+        Self {
+            lines: SCRIPT_PATH.split("\n")
+                .map(|line| line.to_string())
+                .collect(),
+            current_line: 0,
+        }
+    }
+}
+
+impl GameState {
+    fn assess_current_line(&mut self) {
+        match &self.lines[self.current_line] {
+            line if line.starts_with("]*") => self.evaluate_choice(),
+            _ => self.print_current_line(),
+        }
+    }
+    
+    fn print_current_line(&mut self) {
+        text!(self.lines[self.current_line].as_str());
+        
+        // move this maybe into a bespoke input checker?
+        if gamepad(0).start.just_pressed() {
+            self.current_line += 1;
+        }
+    }
+    
+    fn evaluate_choice(&mut self) {
+        // split the current line at the ]*
+        let choices: Vec<String> = self.lines[self.current_line]
+            .split("]*")
+            .map(|choice| choice.trim().to_string())
+            .collect();
+        // draw choice one in one place, choice two in another
+        text!(choices[1].as_str(), x = 0, y = 0);
+        text!(choices[2].as_str(), x = 100, y = 0);
+        // do input check for left or right
+        if gamepad(0).up.just_pressed() {
+            // will have to update this for moving to knots eventually
+            text!("choice one picked", x = 0, y = 50);
+        }
+        else if gamepad(0).down.just_pressed() {
+            // will have to update this for moving to knots eventually
+            text!("choice two picked", x = 0, y = 50);
+        }
+    }
+}
+
+turbo::go! {
+    let mut state = GameState::load();
+    
+    GameState::assess_current_line(&mut state);
+    
+    state.save();
+}
