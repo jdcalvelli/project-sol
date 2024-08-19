3b9523337943c514093c505879a44390f646017d
jdcalvelli <jd@pklwrks.xyz>
2024-08-19 13:57:39 -0400

    refactoring script reading into director module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 mod config;
-mod textbox;
+mod director;
 
 // to get the whole script file
 static SCRIPT_PATH: &str = std::include_str!("../scripts/script.tdsl");
@@ -37,183 +37,6 @@ impl GameState {
                 ("tween_up_cam".to_string(), Tween::new(0.)),
                 ]),
             tween_done_once: false,
-        }
-    }
-    
-    fn assess_current_line(&mut self) {
-        match &self.lines[self.current_line] {
-            line if line.starts_with("<<") || line.starts_with("#") || line == "" => {
-                // is a passage, send, comment, or blank line, so increment on to next line
-                self.current_line += 1;
-            },
-            line if line.starts_with(">>") => {
-                // get divert text value
-                let mut divert_text = self.lines[self.current_line].chars();
-                divert_text.next();
-                divert_text.next();
-                // move to divert area!
-                let new_knot_index: usize = self.lines
-                    .iter()
-                    .position(|line| *line == format!("{}{}", "<< ", divert_text.as_str().trim()))
-                    .unwrap();
-                self.current_line = new_knot_index;
-            },
-            line if line.starts_with("]>") => {
-                // choice logic
-                self.evaluate_choice();
-            },
-            line if line.starts_with("!") => {
-                // command block
-                self.evaluate_command();
-            },
-            line if line.starts_with("-- end") => {
-                // set character to none
-                self.speaking_char = 0;
-                // reset tweens to zero
-                self.tween_done_once = false;
-                // set state to end state
-                self.scene = 2;
-            }
-            _ => {
-                // regular line
-                self.print_current_line();
-            },
-        }
-    }
-    
-    fn print_current_line(&mut self) {
-        // split at the : to get character and line
-        let statement: Vec<String> = self.lines[self.current_line]
-            .split(":")
-            .filter(|&element| element != "")
-            .map(|element| element.trim().to_string())
-            .collect();
-        // draw char portrait
-        match statement[0].as_str() {
-            "LEFT" => self.speaking_char = 1,
-            "RIGHT" => self.speaking_char = 2,
-            _ => {},
-        }
-        
-        // draw textbox
-        textbox::render_textbox(&statement);
-        
-        // move this maybe into a bespoke input checker?
-        if gamepad(0).start.just_pressed() {
-            self.current_line += 1;
-            self.tween_done_once = false;
-        }
-    }
-    
-    fn evaluate_command(&mut self) {
-        // no one talking!
-        self.speaking_char = 0;
-        // increment wait time, like a local tick basically
-        self.wait_timer += 1;
-
-        // split at / for time
-        let command_with_arg: Vec<String> = self.lines[self.current_line]
-            .split("/")
-            .filter(|&element| element != "")
-            .map(|element| element.trim().to_string())
-            .collect();
-        let (command, arg) = (&command_with_arg[0], &command_with_arg[1]);
-
-        match command.as_str() {
-            "! WAIT" => {
-                // dont increment the line for a period of time
-                if self.wait_timer == arg.parse::<u16>().unwrap() * 60 {
-                    self.current_line += 1;
-                    // reset wait_timer
-                    self.wait_timer = 0;
-
-                    self.tween_done_once = false;
-                }
-            },
-            _ => {}
-        }
-    }
-
-    fn evaluate_choice(&mut self) {
-        // split the current line at the ]>
-        let choices: Vec<String> = self.lines[self.current_line]
-            .split("]>")
-            .filter(|&element| element != "")
-            .map(|choice| choice.trim().to_string())
-            .collect();
-        
-        // set speaking character to none for choices
-        self.speaking_char = 0;
-        
-        textbox::render_choice_textbox(&choices);
-        
-        // look forward to next line, split at >> to get diverts
-        let diverts: Vec<String> = self.lines[self.current_line + 1]
-            .split(">>")
-            .filter(|&element| element != "")
-            .map(|divert| divert.trim().to_string())
-            .collect();
-        
-        // do input check for left or right
-        if gamepad(0).left.just_pressed() {
-            if diverts[0] == "NULL" {
-                // pass entirely
-                return;
-            }
-            // search the full script to see where << that is, get that index, set current line to that
-            let new_knot_index: usize = self.lines
-                .iter()
-                .position(|line| *line ==  format!("{}{}", "<< ", diverts[0]))
-                .unwrap();
-            
-            self.current_line = new_knot_index;
-
-            self.tween_done_once = false;
-        }
-        else if gamepad(0).right.just_pressed() {
-            if diverts[1] == "NULL" {
-                // pass entirely
-                return;
-            }
-            // search the full script to see where << that is, get that index, set current line to that
-            let new_knot_index: usize = self.lines
-                .iter()
-                .position(|line| *line ==  format!("{}{}", "<< ", diverts[1]))
-                .unwrap();
-            
-            self.current_line = new_knot_index;
-
-            self.tween_done_once = false;
-        }
-        else if gamepad(0).up.just_pressed() {
-            if diverts[2] == "NULL" {
-                // pass entirely
-                return;
-            }
-            // search the full script to see where << that is, get that index, set current line to that
-            let new_knot_index: usize = self.lines
-                .iter()
-                .position(|line| *line ==  format!("{}{}", "<< ", diverts[2]))
-                .unwrap();
-            
-            self.current_line = new_knot_index;
-
-            self.tween_done_once = false;
-        }
-        else if gamepad(0).down.just_pressed() {
-            if diverts[3] == "NULL" {
-                // pass entirely
-                return;
-            }
-            // search the full script to see where << that is, get that index, set current line to that
-            let new_knot_index: usize = self.lines
-                .iter()
-                .position(|line| *line ==  format!("{}{}", "<< ", diverts[3]))
-                .unwrap();
-            
-            self.current_line = new_knot_index;
-
-            self.tween_done_once = false;
         }
     }
 }
@@ -318,7 +141,7 @@ turbo::go! {
             }
         },
         1 => {
-            GameState::assess_current_line(&mut state);
+            director::assess_current_line(&mut state);
         },
         2 => {
             if !state.tween_done_once {
